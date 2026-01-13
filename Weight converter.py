349bf42e75cfe0f74552468f75b1e0cff1f9350e
Baha Obaid <bahayobaid@gmail.com>
2026-01-13 20:02:03 +0300

    Update Weight converter.py

--- Weight converter.py
+++ Weight converter.py
@@ -6,24 +6,26 @@ def convert_weight():
         weight = float(entry_weight.get())
         unit = entry_unit.get().strip().lower()
 
-        if unit == "l":
+        if unit == "k":  # input is kilograms -> convert to pounds
             result = weight * 2.205
             result_unit = "Lbs"
-        elif unit == "k":
+            label_result.configure(text=f"Your weight in pound is: {round(result, 1)} {result_unit}")
+        elif unit == "l":  # input is pounds -> convert to kilograms
             result = weight / 2.205
             result_unit = "Kgs"
+            label_result.configure(text=f"Your weight in Kilograms is: {round(result, 1)} {result_unit}")
         else:
             messagebox.showerror("Error", "Unit must be K or L")
             return
 
-        label_result.configure(text=f"Your weight is: {round(result, 1)} {result_unit}")
+        
 
     except ValueError:
         messagebox.showerror("Error", "Please enter a valid number")
 
-#UI
-ctk.set_appearance_mode("dark")     
-ctk.set_default_color_theme("blue") 
+# UI
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
 
 app = ctk.CTk()
 app.title("Weight Converter")
@@ -36,7 +38,7 @@ title.pack(pady=(20, 10))
 entry_weight = ctk.CTkEntry(app, placeholder_text="Enter weight (e.g., 70)")
 entry_weight.pack(pady=8, padx=30, fill="x")
 
-entry_unit = ctk.CTkEntry(app, placeholder_text="Unit: K or L")
+entry_unit = ctk.CTkEntry(app, placeholder_text="Unit of your weight: K or L")
 entry_unit.pack(pady=8, padx=30, fill="x")
 
 btn = ctk.CTkButton(app, text="Convert", command=convert_weight)
